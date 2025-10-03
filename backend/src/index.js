8a9ea431de1181d43b754ccc4ef3305a06c0d91d
Amar Gupta <amararya780@gmail.com>
2025-10-03 20:11:45 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173', 'https://algonaut-coding-platform1.onrender.com'],
     credentials: true
 }))
 app.use(express.json());
