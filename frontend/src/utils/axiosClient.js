d89c983503c2a43f42a90798f41611e5b29954f1
Amar2314073 <165203909+Amar2314073@users.noreply.github.com>
2025-10-03 19:30:13 +0530

    Update axiosClient.js

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
  const axiosClient = axios.create({
-    baseURL: 'srv-d3ft7gp5pdvs73bsa2r0',
+    baseURL: 'https://project-algonaut-1.onrender.com',
     withCredentials: true,
     headers:{
         'Content-Type': 'application/json'
