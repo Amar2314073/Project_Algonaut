c512f88ee38f9932c39eee3e128d6c844a488882
Amar Gupta <amararya780@gmail.com>
2025-10-06 23:02:45 +0530

    modified

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,12 +1,17 @@
 
 import axios from "axios";
 
- const axiosClient = axios.create({
-    baseURL: 'https://project-algonaut-4i35.vercel.app',
-    withCredentials: true,
-    headers:{
-        'Content-Type': 'application/json'
-    }
+const isLocal = window.location.hostname === 'localhost';
+
+const axiosClient = axios.create({
+  baseURL: isLocal
+    ? 'http://localhost:3000' // backend running locally
+    : 'https://project-algonaut-4i35.vercel.app', // deployed backend
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
+
 
 export default axiosClient;
