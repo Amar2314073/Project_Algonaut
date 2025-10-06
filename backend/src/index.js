39c88d09a01260d884812e391ce1ced3d27beb70
Amar Gupta <amararya780@gmail.com>
2025-10-06 22:18:22 +0530

    modified

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,8 +14,17 @@ const cors = require('cors');
 
 app.use(cors({
     origin:'https://project-algonaut.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }))
+
+app.options('*', cors({
+    origin: 'https://project-algonaut.vercel.app',
+    credentials: true
+}));
+
+
 app.use(express.json());
 app.use(cookieParser());
 
