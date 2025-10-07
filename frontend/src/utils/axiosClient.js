d1cf15d5faa79d3b15916d9189e85a4df9d52397
Amar Gupta <amararya780@gmail.com>
2025-10-07 19:16:07 +0530

    update

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -5,7 +5,7 @@ const isLocal = window.location.hostname === 'localhost';
 
 const axiosClient = axios.create({
   baseURL: isLocal
-    ? 'http://localhost:3000' // backend running locally
+    ? 'https://localhost:3000' // backend running locally
     : 'https://project-algonaut-1.onrender.com', // deployed backend'
   withCredentials: true,
   headers: {
